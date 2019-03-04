ebab1f97ca96424c7ab50536cc144a69f8e915e1
Rido <rmpablos@gmail.com>
2019-03-04 08:01:51 +0000

    change title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Rido Blog
           </Link>
         </h1>
       )
